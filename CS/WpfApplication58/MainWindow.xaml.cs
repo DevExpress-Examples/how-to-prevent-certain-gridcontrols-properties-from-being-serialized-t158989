03528e153e9017bbe4c44be96e9135904a96e57e
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-09 10:38:13 +0400

    Update sample

--- CS/WpfApplication58/MainWindow.xaml.cs
+++ CS/WpfApplication58/MainWindow.xaml.cs
@@ -1,70 +1,34 @@
 ﻿using DevExpress.Xpf.Core.Serialization;
-using DevExpress.Xpf.Editors;
 using DevExpress.Xpf.Grid;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApplication58
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace WpfApplication58 {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
-
             ObservableCollection<Customer> customers = new ObservableCollection<Customer>();
-            for (int i = 1; i < 30; i++)
-            {
+            for (int i = 1; i < 30; i++) {
                 customers.Add(new Customer() { ID = i, Name = "Name" + i });
             }
             grid.ItemsSource = customers;
-            grid.Columns["ID"].AddHandler(DXSerializer.AllowPropertyEvent, new AllowPropertyEventHandler(OnAllowProperty));
 
+            grid.Columns[nameof(Customer.ID)].AddHandler(DXSerializer.AllowPropertyEvent, new AllowPropertyEventHandler(OnAllowProperty));
         }
-        void OnAllowProperty(object sender, AllowPropertyEventArgs e)
-        {
-            if (e.DependencyProperty == GridColumn.ActualWidthProperty)
+        void OnAllowProperty(object sender, AllowPropertyEventArgs e) {
+            if (e.DependencyProperty == GridColumn.WidthProperty)
                 e.Allow = false;
         }
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            grid.RestoreLayoutFromXml("layout.xml");
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
+        void OnSaveLayout(object sender, RoutedEventArgs e) {
             grid.SaveLayoutToXml("layout.xml");
+        }
+        void OnRestoreLayout(object sender, RoutedEventArgs e) {
+            grid.RestoreLayoutFromXml("layout.xml");
         }
     }
 
-    public class Customer
-    {
-        public int ID
-        {
-            get;
-            set;
-        }
-
-        public string Name
-        {
-            get;
-            set;
-        }
+    public class Customer {
+        public int ID { get; set; }
+        public string Name { get; set; }
     }
 }
